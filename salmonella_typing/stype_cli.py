1b3621bbad7e49fb82b9c5952b35da74ae1f47d9
Anders Goncalves da Silva <andersgs@gmail.com>
2018-08-26 21:57:03 -0700

    CLI now has all three commands

--- salmonella_typing/stype_cli.py
+++ salmonella_typing/stype_cli.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from scripts.SistrTyping import SistrTyping
+from scripts.SistrTyping import RunSistrWorkflow, CleanSistrWorkflow, UnlockSistrWorkflow
 from cleo import Application
 
 application = Application('stype_cli', '0.1.0')
-application.add(SistrTyping())
+application.add(RunSistrWorkflow())
+application.add(CleanSistrWorkflow())
+application.add(UnlockSistrWorkflow())
 
 if __name__ == '__main__':
     application.run()
