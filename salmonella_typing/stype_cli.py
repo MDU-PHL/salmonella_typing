c857df936afb9e14f007b787dc57b8878e2814b8
Anders Goncalves da Silva <andersgs@gmail.com>
2018-10-10 13:36:50 -0700

    Patched in LOD experiment  subcommand

--- salmonella_typing/stype_cli.py
+++ salmonella_typing/stype_cli.py
@@ -3,6 +3,7 @@
 
 from scripts.SistrTyping import RunSistrWorkflow, CleanSistrWorkflow, UnlockSistrWorkflow
 from scripts.SistrParse import ParseSistrOuput
+from validation.limitOfDetection.LODExperimentWorkflow import LODCommand
 from cleo import Application
 
 application = Application('stype_cli', '0.1.0')
@@ -10,6 +11,7 @@ application.add(RunSistrWorkflow())
 application.add(CleanSistrWorkflow())
 application.add(UnlockSistrWorkflow())
 application.add(ParseSistrOuput())
+application.add(LODCommand())
 
 if __name__ == '__main__':
     application.run()
