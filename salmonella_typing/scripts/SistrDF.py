ac21ce593145383332fe2a05db156a7687008ff0
Anders Goncalves da Silva <andersgs@gmail.com>
2018-12-04 15:47:48 -0800

    Made o-antigen column go before h1, and h2

--- salmonella_typing/scripts/SistrDF.py
+++ salmonella_typing/scripts/SistrDF.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 MDUIDREG = re.compile(r'(?P<id>[0-9]{4}-[0-9]{5})-?(?P<itemcode>.{1,2})?')
 
+
 @pd.api.extensions.register_dataframe_accessor("mms136")
 class SistrDF(object):
     '''
@@ -39,7 +40,7 @@ class SistrDF(object):
         new_tab = self._obj.apply(self._split_id, axis=1, result_type='expand')
         self._obj.insert(0, 'ITEMCODE', new_tab.ITEMCODE)
         self._obj.insert(0, 'ID', new_tab.ID)
-    
+
     def apply_rules(self, rule_list, criteria):
         '''
         Given a list of tuples (rule_name, function), add masking columns
@@ -57,14 +58,14 @@ class SistrDF(object):
         if the sample is True for only one criteria. If sum is different from 0, 
         the sample needs to be reviewed
         '''
-        for rule,func in rule_list:
+        for rule, func in rule_list:
             self._obj[rule] = func(self._obj)
-        
+
         for k in criteria:
             self._obj[k] = self._obj.eval(criteria[k])
 
         self._obj['CONSISTENT'] = self._obj[list(criteria)].sum(axis=1)
-    
+
     def apply_filters(self, filter_list):
         '''
         Similar to rules above, given a list of tuples (filter_name, function), add 
@@ -76,18 +77,20 @@ class SistrDF(object):
         '''
         self._obj['serovar-original'] = self._obj.serovar
         filt_list = []
-        for filt,func in filter_list:
+        for filt, func in filter_list:
             self._obj[filt] = func(self._obj)
             filt_list.append(filt)
-        self._obj['FILTERS'] = self._obj[filt_list].apply(lambda x: sum(~x.isnull()), axis=1)
-        self._obj['serovar'] = self._obj.apply(lambda x: x.serovar if x[filt_list].isnull().all() else x[filt_list].dropna()[0], axis = 1)
-        
-    
+        self._obj['FILTERS'] = self._obj[filt_list].apply(
+            lambda x: sum(~x.isnull()), axis=1)
+        self._obj['serovar'] = self._obj.apply(lambda x: x.serovar if x[filt_list].isnull(
+        ).all() else x[filt_list].dropna()[0], axis=1)
+
     def call_status(self):
         '''
         '''
         PASS = self._obj['PASS']
-        REVIEW =  self._obj.loc[:,self._obj.columns.str.startswith('REVIEW')].any(axis = 1)
+        REVIEW = self._obj.loc[:, self._obj.columns.str.startswith(
+            'REVIEW')].any(axis=1)
         FAIL = self._obj['FAIL']
         EDGE = self._obj['EDGE']
         CONSISTENT = self._obj['CONSISTENT'] == 1
@@ -99,8 +102,9 @@ class SistrDF(object):
             (FAIL & CONSISTENT & FILTERS_OK)
         ]
         choices = ['PASS', 'REVIEW', 'REVIEW, EDGE', 'FAIL']
-        self._obj['STATUS'] = np.select(conditions, choices, default='REVIEW, INCONSISTENT')
-    
+        self._obj['STATUS'] = np.select(
+            conditions, choices, default='REVIEW, INCONSISTENT')
+
     def output_csv(self, outname="sistr_out.csv", summary=True):
         '''
         The off chance someone wants a CSV output. It will have the following
@@ -112,14 +116,16 @@ class SistrDF(object):
         Otherwise, output the complete table (minus ID and ITEMCODE)
         '''
         if summary:
-            output_cols = ['SEQID', 'cgmlst_subspecies','serovar', 'h1', 'h2', 'o_antigen', 'STATUS']
+            output_cols = ['SEQID', 'cgmlst_subspecies',
+                           'serovar', 'h1', 'h2', 'o_antigen', 'STATUS']
         else:
             output_cols = self._obj.columns
-        
-        outtab = self._obj.loc[:,output_cols]
-        outtab.rename(columns = {'cgmlst_subspecies': 'subspecies'}, inplace=True)
+
+        outtab = self._obj.loc[:, output_cols]
+        outtab.rename(
+            columns={'cgmlst_subspecies': 'subspecies'}, inplace=True)
         outtab.to_csv(outname, index=False)
-    
+
     def output_lims(self, outname="sistr_lims.xlsx"):
         '''
         Generate a LIMS output
@@ -130,14 +136,15 @@ class SistrDF(object):
         - ALL sheet with the full output analysis
         '''
         summary_header = ['ID', 'ITEMCODE', 'SEQID',
-            'cgmlst_subspecies', 'cgmlst_matching_alleles', 'serovar_cgmlst',
-            'h1', 'h2', 'o_antigen', 'serogroup', 'serovar_antigen', 'serovar-original',
-            'serovar', 'STATUS'
-        ]
+                          'cgmlst_subspecies', 'cgmlst_matching_alleles', 'serovar_cgmlst',
+                          'o_antigen', 'h1', 'h2', 'serogroup', 'serovar_antigen', 'serovar-original',
+                          'serovar', 'STATUS'
+                          ]
         writer = pd.ExcelWriter(outname, engine='xlsxwriter')
-        pass_df = self._obj.query("STATUS == 'PASS'").loc[:,summary_header]
+        pass_df = self._obj.query("STATUS == 'PASS'").loc[:, summary_header]
         pass_df.to_excel(writer, sheet_name='MMS136', index=False)
-        review_df = self._obj.query("STATUS != 'PASS'").loc[:,summary_header].sort_values(by="STATUS", ascending=False)
+        review_df = self._obj.query("STATUS != 'PASS'").loc[:, summary_header].sort_values(
+            by="STATUS", ascending=False)
         review_df.to_excel(writer, sheet_name='REVIEW', index=False)
         self._obj.to_excel(writer, sheet_name="ALL", index=False)
         writer.save()
@@ -148,7 +155,8 @@ class SistrDF(object):
         '''
         m = MDUIDREG.match(row.SEQID)
         if m is None:
-            logger.warning("The ID do not appear to follow MDU ID standards. Using SEQID directly.")
+            logger.warning(
+                "The ID do not appear to follow MDU ID standards. Using SEQID directly.")
             mduid = row.SEQID
             itemcode = ''
         else:
