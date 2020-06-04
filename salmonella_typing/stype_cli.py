96102c2b70ab9627598989d191c8430fb5f63153
Anders Goncalves da Silva <andersgs@gmail.com>
2020-06-04 14:28:55 -0700

    Add TestWorkflow command option

--- salmonella_typing/stype_cli.py
+++ salmonella_typing/stype_cli.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from scripts.SistrTyping import RunSistrWorkflow, CleanSistrWorkflow, UnlockSistrWorkflow
-from scripts.SistrParse import ParseSistrOuput
-from validation.limitOfDetection.LODExperimentWorkflow import LODCommand
+from .scripts.SistrTyping import (
+    RunSistrWorkflow,
+    CleanSistrWorkflow,
+    UnlockSistrWorkflow,
+    TestSistrWorkflow,
+)
+from .scripts.SistrParse import ParseSistrOutput
+from .validation.limitOfDetection.LODExperimentWorkflow import LODCommand
 from cleo import Application
 
-application = Application('stype_cli', '0.1.0')
+application = Application("stype_cli", "0.1.1")
 application.add(RunSistrWorkflow())
+application.add(TestSistrWorkflow())
 application.add(CleanSistrWorkflow())
 application.add(UnlockSistrWorkflow())
-application.add(ParseSistrOuput())
-#application.add(LODCommand())
+application.add(ParseSistrOutput())
+# application.add(LODCommand())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application.run()
